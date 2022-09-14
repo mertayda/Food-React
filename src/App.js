9a2305622b18377560ecff8072f6cbf159e6af6b
Mert Ayda <48617781+mertayda@users.noreply.github.com>
2022-09-14 14:17:21 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import HeadlineCards from './Components/HeadlineCards';
+import Food from './Components/Food';
+import Category from './Components/Category';
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <HeadlineCards></HeadlineCards>
+      <Food></Food>
+      <Category></Category>
+    </div>
+  );
+}
+
+export default App;
